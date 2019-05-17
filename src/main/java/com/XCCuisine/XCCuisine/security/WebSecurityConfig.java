60547b555b819b1e6f9e75d94c5a947646f2f725
xbtshady <xx943217258>
2019-05-17 10:23:24 +0800

    Security配置

---
+++ src/main/java/com/XCCuisine/XCCuisine/security/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.XCCuisine.XCCuisine.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeRequests()
+                .antMatchers("/index").permitAll()//访问index.html不要权限验证
+                .anyRequest().authenticated()//其他所有路径都需要权限校验
+                .and()
+                .csrf().disable()//默认开启，这里先显式关闭
+                .formLogin()  //内部注册 UsernamePasswordAuthenticationFilter
+                .loginPage("/login") //表单登录页面地址
+                .loginProcessingUrl("/login")//form表单POST请求url提交地址，默认为/login
+                .permitAll();//允许所有用户都有权限访问登录页面
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        UserDetails user = User.withUsername("username")
+                .password(encoder.encode("password"))
+                .roles("USER").build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
